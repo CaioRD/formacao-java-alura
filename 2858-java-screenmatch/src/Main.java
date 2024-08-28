81e5d6d7c951e22060e04c6572f21b1b3a2a5310
CaioRD <caiords67@gmail.com>
2024-08-28 17:48:16 -0300

    add commit

--- 2858-java-screenmatch/src/Main.java
+++ 2858-java-screenmatch/src/Main.java
@@ -1,5 +1,13 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Bem vindo ao Screen Match");
+        double celcius = 25.5;
+        double fahrenheight = (celcius * 1.8) + 32;
+
+        String mensagem = String.format("A temperatura de %2f Celcius é equivalente a %2f fahrenheight ", celcius, fahrenheight);
+        System.out.println(mensagem);
+
     }
 }
+
